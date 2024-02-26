6ddbd1eac14e6eec1bc80d397fc327a1ce8359d0
Anas Hamrouni <anas.hamrouni@rwth-aachen.de>
2024-02-26 11:52:18 +0100

    fixing

--- youtubebot.py
+++ youtubebot.py
@@ -253,7 +253,7 @@ async def play(ctx: commands.Context, *args):
                     if 'entries' in info:
                          info = info['entries'][0]
                     # send link if it was a search, otherwise send title as sending link again would clutter chat with previews
-                    await ctx.send('adding ' + (f'https://youtu.be/{info["id"]}' if will_need_search else f'`{info["title"]}`' +' to queue'))
+                    await ctx.send('adding ' + (f'https://youtu.be/{info["id"]} to the queue' if will_need_search else f'`{info["title"]}` to the queue'))
                     try:
                          ydl.download([query])
                     except yt_dlp.utils.DownloadError as err:
