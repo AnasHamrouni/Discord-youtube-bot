0d2060e4307e143b1ac4d08c6a47a348a4655382
Anas Hamrouni <anas.hamrouni@rwth-aachen.de>
2024-07-14 16:30:51 +0200

    update

--- youtubebot.py
+++ youtubebot.py
@@ -113,7 +113,10 @@ async def queue(ctx: commands.Context, *args):
                     "-p 27036:27036/udp "
                     "-v /home/ubuntu/7daystodie/server:/home/steam/server "
                     "-v /home/ubuntu/7daystodie/saves:/home/steam/.local/share "
-                    "7days"
+                    "--env VM_MAX_MAP_COUNT=2262144 "
+                    "--sysctl vm.max_map_count=2262144 "
+                    "--ulimit core=-1 "
+                    "7days2"
                )
 
                # Start the specified Docker container
