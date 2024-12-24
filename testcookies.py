931e02c95cd1874dae06fac6ed3494d4d313c8ff
anashamrouni <xmranasx@email.com>
2024-12-24 23:30:05 +0100

    test

--- testcookies.py
+++ testcookies.py
@@ -1,13 +1,14 @@
 import subprocess
+import yt_dlp
 
 # Define the command
 command = 'yt-dlp --cookies ./cookies.txt https://www.youtube.com/watch?v=4w9iDLuvSOQ -vU'
-
-# Execute the command
-try:
-    result = subprocess.run(command, shell=True, check=True, text=True)
-    print("Command executed successfully!")
-    print("Output:", result.stdout)
-except subprocess.CalledProcessError as e:
-    print(f"Error executing command: {e}")
-    print("Error Output:", e.stderr)
+print(yt_dlp.__version__)
+# # Execute the command
+# try:
+#     result = subprocess.run(command, shell=True, check=True, text=True)
+#     print("Command executed successfully!")
+#     print("Output:", result.stdout)
+# except subprocess.CalledProcessError as e:
+#     print(f"Error executing command: {e}")
+#     print("Error Output:", e.stderr)
