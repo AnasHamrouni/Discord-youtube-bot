bb795f8cd5eba1b349b7f324e2eafeabf784eb65
anashamrouni <xmranasx@email.com>
2024-12-24 23:18:09 +0100

    test

--- testcookies.py
+++ testcookies.py
@@ -9,7 +9,7 @@ ytdl_opts = {
 }
 
 # Pass the options to YoutubeDL
-with yt_dlp(ytdl_opts) as ydl:
+with yt_dlp.YoutubeDL(ytdl_opts) as ydl:
     # Extract video information
     info = ydl.extract_info("https://www.youtube.com/watch?v=us8gGsb9ZV8", download=False)
     print(info)
