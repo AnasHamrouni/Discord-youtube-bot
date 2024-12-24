70b948851282fa46e22c4f7cded4fa3aac1669e2
anashamrouni <xmranasx@email.com>
2024-12-24 23:15:40 +0100

    test

---
+++ testcookies.py
@@ -0,0 +1,15 @@
+import yt_dlp
+
+# Define the options
+ytdl_opts = {
+    'format': 'best',
+    'noplaylist': True,
+    'cookies' : '/home/ubuntu/omkelthoum/cookies.txt',
+    'verbose': True,
+}
+
+# Pass the options to YoutubeDL
+with yt_dlp(ytdl_opts) as ydl:
+    # Extract video information
+    info = ydl.extract_info("https://www.youtube.com/watch?v=us8gGsb9ZV8", download=False)
+    print(info)
